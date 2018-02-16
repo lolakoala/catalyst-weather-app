431fd4801cc5bb48ddbef2982955e21df2889fe0
Lola Brenner <amandadbrenner@gmail.com>
2018-02-16 09:25:07 -0700

    adds check to see if city has already been searched, to cache info

--- src/App.js
+++ src/App.js
@@ -24,7 +24,6 @@ class App extends Component {
     if ("geolocation" in navigator) {
       navigator.geolocation.getCurrentPosition(position => {
         getWeatherWithCoord(position)
-        // .then(res => console.log(res.city))
           .then(res => {
             this.setState({
               currentLocation: res.city.name,
@@ -40,18 +39,30 @@ class App extends Component {
 
   setLocation = currentLocation => {
     this.setState({ currentLocation });
-    this.setWeather(currentLocation);
+
+    const allSearchedLocations = this.state.weather.map(cityAndWeather => {
+      return Object.keys(cityAndWeather).toString();
+    });
+
+    if (!allSearchedLocations.includes(currentLocation)) {
+      this.setWeather(currentLocation);
+    }
   }
 
 
   render() {
-    // handle waiting on info to come back
-    return (
-      <div className="App">
-        <p>`Your current location is {this.state.currentLocation}.`</p>
-        <Controls setLocation={this.setLocation}/>
-      </div>
-    );
+    if (!this.state.currentLocation.length) {
+      return (<p>Please wait while Simple Weather loads your current weather.</p>);
+    } else {
+      return (
+        <div className="App">
+          <p>Welcome to Simple Weather.</p>
+          <p>Your current location is {this.state.currentLocation}.</p>
+          <Controls setLocation={this.setLocation}/>
+          <Graph />
+        </div>
+      );
+    }
   }
 }
 
