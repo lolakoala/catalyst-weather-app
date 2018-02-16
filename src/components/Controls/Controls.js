21d09fa9b3900fc87c3161714bbef960442e7f95
Lola Brenner <amandadbrenner@gmail.com>
2018-02-16 07:53:56 -0700

    adds state to controls

--- src/components/Controls/Controls.js
+++ src/components/Controls/Controls.js
@@ -2,9 +2,18 @@ import React, { Component } from 'react';
 import PropTypes from 'prop-types';
 
 class Controls extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      input: ''
+    };
+  }
+
   render() {
     return (<div>
-      <input type="text" />
+      <input type="text"
+        value={this.state.input}
+        onChange={(event) => this.setState({ input: event.target.value })}/>
       <button>Change Location</button>
     </div>);
   }
